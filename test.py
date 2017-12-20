c4185bad140e3f4c864b87c79dd7dc4574add9a2
mudox <imudox@gmail.com>
2017-12-20 08:36:46 +0800

    Test script now wait for user command to log out

--- test.py
+++ test.py
@@ -3,32 +3,68 @@
 
 import logging
 import random
-from time import sleep
 
 from faker import Faker
+
 from jaclog import jaclog
 
 jaclog.configure(appName='test_jaclog', fileName='test.log', compact=True)
 faker = Faker()
 
-lastLogFunction = None
-while True:
-  sleep(random.randrange(0, 19) / 10)
+lastLogFunction = logger = logging.getLogger(__name__).info
+lastLogFunction('this is jaclog test utiity')
 
-  subsystem = '.'.join(faker.words(nb=random.randrange(1, 4)))
-  logger = logging.getLogger(subsystem)
+try:
+  while True:
+    subsystem = '.'.join(faker.words(nb=random.randrange(1, 4)))
+    logger = logging.getLogger(subsystem)
 
-  logFunctions = [logger.error, logger.warning, logger.info, logger.debug]
-  log = random.choice(logFunctions)
+    logFunctions = [logger.error, logger.warning, logger.info, logger.debug]
+    log = random.choice(logFunctions)
 
-  lineCount = random.randrange(1, 5)
-  lines = ''
-  for _ in range(lineCount):
-    lines += '\n' + faker.sentence(nb_words=7)
+    lineCount = random.randrange(1, 4)
+    lines = ''
+    for _ in range(lineCount):
+      lines += '\n' + faker.sentence(nb_words=7)
+    lines = lines.strip()
+    kind = input('kind of message content [c|o|mo|co|cmo]:')
 
-  if random.randrange(4) == 0 and lastLogFunction is not None:
-    lastLogFunction(lines)
-  else:
-    lastLogFunction = log
+    print(f'>> org lines:\n{lines}')
+
+    if kind == 'o':
+      lines = 'o:  🦁  [o]'
+      continued = False
+
+    elif kind == 'mo':
+      lines = f'o:  🐸  [mo]\n{lines}'
+      continued = False
+
+    if kind == 'co':
+      lines = 'o:  🐵  [co]'
+      continued = True
+
+    elif kind == 'cmo':
+      lines = f'o:  🐼  [cmo]\n{lines}'
+      continued = True
+
+    elif kind == 'c':
+      continued = True
+
+    else:
+      continued = False
+
+    dice = random.choices(
+        ['bare one liner', 'multiline one liner', 'normal'],
+        weights=[2, 5, 7]
+    )[0]
+
+    print(f'>> lines:\n{lines}')
+
+    if continued:
+      log = lastLogFunction
+
     log(lines)
+    lastLogFunction = log
 
+except KeyboardInterrupt:
+  exit(0)
